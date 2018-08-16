a2f856d747cd869a952c17405371e663e42311ec
Roland Chelwing <jolleee@hotmail.com>
2018-08-16 11:41:25 +0200

    xdss

---
+++ Core/Variables/BoolVariable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Variables/Bool")]
+public class BoolVariable : ScriptableObject
+{
+  public bool defaultValue;
+  public bool currentValue;
+  void OnEnable() { currentValue = defaultValue; }
+}
---
+++ EventSystem/Editor/GameEventEditor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(GameEvent))]
+public class GameEventEditor : Editor
+{
+  public override void OnInspectorGUI()
+  {
+    base.OnInspectorGUI();
+    GUI.enabled = Application.isPlaying;
+    GameEvent e = (GameEvent)target;
+    if (GUILayout.Button("Raise")) e.Raise();
+  }
+}
