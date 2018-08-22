9dc6d57190a095e0cb0c807c12bb878cb0aa31b8
Roland Chelwing <jolleee@hotmail.com>
2018-08-22 12:17:46 +0200

    xdss

--- Battle/Ability/Skill/Skill.cs
+++ Battle/Ability/Skill/Skill.cs
@@ -1,21 +1,20 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Ability : ScriptableObject
+public abstract class Skill : ScriptableObject
 {
   public Blocker blocker;
 
   [Header("Settings")]
-  public int abilityRange = 2;
+  public float abilityRange = 1.5f;
   public float cooldown = 2;
+  public float castTime = 1f;
   public List<AbilityEffect> effects = new List<AbilityEffect>();
   [Space(10)]
 
   [Header("Visuals")]
   public AnimationTrigger animationTrigger;
-  public ParticleSystem startEffect;
-  public ParticleSystem onCastEffect;
-  
+
   [HideInInspector] public float lastAction = 0;
   public GameObject caster { get; set; }
   public bool isReady { get { return Time.time > lastAction; } }
---
+++ Battle/Ability/Skill/TargetSkill.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+
+[CreateAssetMenu(menuName = "Skills/Target")]
+public class TargetSkill : AISkill
+{
+  public override void Setup(GameObject casterGo, int targetLayerParam) { base.Setup(casterGo, targetLayerParam); }
+  public override void Cast(GameObject target)
+  {
+    base.Cast(target);
+    TargetCast();
+  }
+  void TargetCast() { }
+}
